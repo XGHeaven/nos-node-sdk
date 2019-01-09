50019f67d22dcdf17907bb7f2c2b5787ac60c1fb
XGHeaven <xgheaven@gmail.com>
2019-01-09 14:27:25 +0800

    style: remove unused code

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,35 +1,6 @@
 import * as xml from 'fast-xml-parser'
-import fetch, { BodyInit, Response } from 'node-fetch'
+import { Response } from 'node-fetch'
 import { camelCaseObject } from './util'
-
-export async function request(method: string, url: string, headers: any, body?: BodyInit) {
-  const resp = await requestResponse(method, url, headers, body)
-  console.log(resp.status)
-  const xmlString = await resp.text()
-  return xml.parse(xmlString, {
-    // don't parse number
-    parseTrueNumberOnly: true,
-  })
-}
-
-export async function requestStream(
-  method: string,
-  url: string,
-  headers: any,
-  body?: BodyInit
-): Promise<NodeJS.ReadableStream> {
-  const resp = await requestResponse(method, url, headers, body)
-
-  return resp.body
-}
-
-export async function requestResponse(method: string, url: string, headers: any, body?: BodyInit): Promise<Response> {
-  return await fetch(url, {
-    method,
-    headers,
-    body,
-  })
-}
 
 export async function parseBody(resp: Response): Promise<any> {
   const xmlString = await resp.text()
